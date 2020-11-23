e16148a51a99d493c9a2b5f3faef06f3369440c3
Jitendra Patel <41681183+ammipatel@users.noreply.github.com>
2020-11-23 13:13:40 +0530

    [Jitendra]Added: Create a simple html file to test the REST services with JSON server using promise

---
+++ js/http_services.js
@@ -0,0 +1,28 @@
+function makePromiseCall(methodType, url, async = true, data = null) {
+    return new Promise(function (resolve, reject) {
+      let xhr = new XMLHttpRequest();
+      xhr.onload = function () {
+        if (xhr.readyState == 4) {
+          if (xhr.status == 200 || xhr.status == 201 || xhr.status == 304) {
+            resolve(xhr.responseText);
+          } else if (xhr.status >= 400) {
+            reject({
+              status: xhr.status,
+              statusText: xhr.statusText,
+            });
+          }
+        }
+      };
+      xhr.onerror = function () {
+        reject({
+          status: this.status,
+          statusText: this.statusText,
+        });
+      };
+      xhr.open(methodType, url, async);
+      if (data) {
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.send(JSON.stringify(data));
+      } else xhr.send();
+    });
+  }
